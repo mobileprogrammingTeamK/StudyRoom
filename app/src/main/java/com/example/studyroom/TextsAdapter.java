453bdde457557fa26f2476cf6462dfa45e11de02
Tsa2 <src.tsa2@gmail.com>
2019-11-24 14:50:08 +0900

    test

--- app/src/main/java/com/example/studyroom/TextsAdapter.java
+++ app/src/main/java/com/example/studyroom/TextsAdapter.java
@@ -87,7 +87,7 @@ public class TextsAdapter extends ArrayAdapter<Texts> {
                     createBtn.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            
+                            //database testss
                         }
                     });
                     Button cancelBtn = dialog.findViewById(R.id.cancelBtn);
